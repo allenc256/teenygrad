dfc5d3c4acbbab0e2daac8df0671118327681960
allenc256 <allenc256@yahoo.com>
2023-08-14 20:21:14 -0700

    linting / style

--- teenygrad/nn.py
+++ teenygrad/nn.py
@@ -1,9 +1,13 @@
-import numpy as np
 from typing import Sequence
+
+import numpy as np
+
 from teenygrad.tensor import Tensor
+
 
 class Module:
     """Analog of `torch.nn.Module`."""
+
     def forward(self, input: Tensor) -> Tensor:
         """Analog of `torch.nn.Module.forward()`."""
         raise NotImplementedError()
@@ -17,8 +21,10 @@ class Module:
         for p in self.parameters():
             p.grad = None
 
+
 class Sequential(Module):
     """Analog of `torch.nn.Sequential`."""
+
     _layers: list[Module]
 
     def __init__(self, *args: Module) -> None:
@@ -26,22 +32,30 @@ class Sequential(Module):
 
     def forward(self, x: Tensor) -> Tensor:
         activations = x
-        for l in self._layers:
-            activations = l.forward(activations)
+        for layer in self._layers:
+            activations = layer.forward(activations)
         return activations
 
     def parameters(self) -> Sequence[Tensor]:
-        return [p for l in self._layers for p in l.parameters()]
+        return [p for layer in self._layers for p in layer.parameters()]
+
 
 class Linear(Module):
     """Analog of `torch.nn.Linear`."""
+
     _weights: Tensor
     _biases: Tensor
 
     def __init__(self, in_features: int, out_features: int) -> None:
-        # N.B., initialize all weights/biases using a uniform distribution over [-1, 1].
-        self._weights = Tensor(np.random.rand(in_features, out_features) * 2 - 1, requires_grad=True)
-        self._biases = Tensor(np.random.rand(out_features) * 2 - 1, requires_grad=True)
+        # N.B., initialize all weights/biases using a uniform distribution over
+        # [-1, 1].
+        self._weights = Tensor(
+            np.random.rand(in_features, out_features) * 2 - 1,
+            requires_grad=True,
+        )
+        self._biases = Tensor(
+            np.random.rand(out_features) * 2 - 1, requires_grad=True
+        )
 
     def forward(self, input: Tensor) -> Tensor:
         return input @ self._weights + self._biases
@@ -49,16 +63,20 @@ class Linear(Module):
     def parameters(self) -> Sequence[Tensor]:
         return [self._weights, self._biases]
 
+
 class ReLU(Module):
     """Analog of `torch.nn.ReLU`."""
+
     def forward(self, input: Tensor) -> Tensor:
         return input.relu()
 
     def parameters(self) -> Sequence[Tensor]:
         return []
 
+
 class Sigmoid(Module):
     """Analog of `torch.nn.Sigmoid`."""
+
     def forward(self, input: Tensor) -> Tensor:
         return input.sigmoid()
 
--- teenygrad/tensor.py
+++ teenygrad/tensor.py
@@ -1,50 +1,62 @@
+from typing import Callable, Sequence
+
 import numpy as np
 from numpy.typing import ArrayLike
-from typing import Sequence, Callable
+
 
 class Tensor:
     """Analog of `torch.Tensor`.
 
-    This class stores all internal values and gradients as numpy arrays. This class includes a simple autograd
-    implementation for supporting back-propagation over various operations over tensors.
+    This class stores all internal values and gradients as numpy arrays. This
+    class includes a simple autograd implementation for supporting back-
+    propagation over various operations over tensors.
 
     Attributes:
         name: The name of the tensor (for debugging purposes).
         value: The value of the tensor.
-        grad: The current gradient of the tensor relative to the scalar loss function, if it has been calculated.
-            This gradient should have the same shape as the value array. If it has not been calculated, this will
-            be set to None.
+        grad: The current gradient of the tensor relative to the scalar loss
+            function, if it has been calculated. This gradient should have the
+            same shape as the value array. If it has not been calculated, this
+            will be set to None.
     """
+
     name: str
     value: np.ndarray
     grad: np.ndarray | None
     _requires_grad: bool
-    _backward_fn: Callable[['Tensor'], None] | None
-    _ancestors: list['Tensor']
+    _backward_fn: Callable[["Tensor"], None] | None
+    _ancestors: list["Tensor"]
 
     def __init__(
         self,
         value: ArrayLike,
         requires_grad: bool | None = None,
-        name: str = '',
-        _backward_fn: Callable[['Tensor'], None] | None = None,
-        _ancestors: Sequence['Tensor'] = (),
+        name: str = "",
+        _backward_fn: Callable[["Tensor"], None] | None = None,
+        _ancestors: Sequence["Tensor"] = (),
     ) -> None:
         """Initializes the tensor.
 
         Args:
             value: The initial value for the tensor.
-            requires_grad: Whether gradients should be computed for the tensor (e.g., whether the tensor corresponds
-                to weights or constant data).
+            requires_grad: Whether gradients should be computed for the tensor
+                (e.g., whether the tensor corresponds to weights or constant
+                data).
             name: The name of the tensor.
         """
         # Validate that requires_grad and _backward_fn are set correctly.
         ancestors_requires_grad = any(a._requires_grad for a in _ancestors)
         if ancestors_requires_grad:
-            if requires_grad == False:
-                raise ValueError('requires_grad cannot be False if at least one ancestor requires gradients')
+            if requires_grad is not None and not requires_grad:
+                raise ValueError(
+                    "requires_grad cannot be False if at least one ancestor "
+                    "requires gradients"
+                )
             if _backward_fn is None:
-                raise ValueError('_backward_fn must be set if at least one ancestor requires gradients')
+                raise ValueError(
+                    "_backward_fn must be set if at least one ancestor "
+                    "requires gradients"
+                )
 
         self.name = name
         self.value = np.copy(value)
@@ -61,207 +73,243 @@ class Tensor:
     def ndim(self) -> int:
         return self.value.ndim
 
-    def sum(self) -> 'Tensor':
+    def sum(self) -> "Tensor":
         """Analog of `torch.Tensor.sum()`."""
-        def _backward_fn(out: 'Tensor') -> None:
+
+        def _backward_fn(out: "Tensor") -> None:
             if self.grad is not None:
                 self.grad += out.grad
 
         return Tensor(
             np.copy(np.sum(self.value)),
-            name='sum',
+            name="sum",
             _ancestors=[self],
             _backward_fn=_backward_fn,
         )
 
-    def log(self) -> 'Tensor':
+    def log(self) -> "Tensor":
         """Analog of `torch.Tensor.log()`."""
-        def _backward_fn(out: 'Tensor') -> None:
+
+        def _backward_fn(out: "Tensor") -> None:
             if self.grad is not None:
                 self.grad += out.grad / self.value
 
         return Tensor(
             np.log(self.value),
-            name='log',
+            name="log",
             _ancestors=[self],
             _backward_fn=_backward_fn,
         )
 
-    def exp(self) -> 'Tensor':
+    def exp(self) -> "Tensor":
         """Analog of `torch.Tensor.exp()`."""
-        def _backward_fn(out: 'Tensor') -> None:
+
+        def _backward_fn(out: "Tensor") -> None:
             if self.grad is not None:
                 self.grad += np.exp(self.value) * out.grad
 
         return Tensor(
             np.exp(self.value),
-            name='exp',
+            name="exp",
             _ancestors=[self],
             _backward_fn=_backward_fn,
         )
 
-    def pow(self, other: float | int) -> 'Tensor':
+    def pow(self, other: float | int) -> "Tensor":
         """Analog of `torch.Tensor.pow()`."""
-        def _backward_fn(out: 'Tensor') -> None:
+
+        def _backward_fn(out: "Tensor") -> None:
             assert out.grad is not None
             if self.grad is not None:
-                self.grad += _unbroadcast_gradient((other * self.value ** (other - 1)) * out.grad, self.grad.shape)
+                self.grad += _unbroadcast_gradient(
+                    (other * self.value ** (other - 1)) * out.grad,
+                    self.grad.shape,
+                )
 
         return Tensor(
-            self.value ** other,
-            name='pow',
+            self.value**other,
+            name="pow",
             _ancestors=[self],
             _backward_fn=_backward_fn,
         )
 
     def relu(self):
         """Analog of `torch.Tensor.relu()`."""
-        def _backward_fn(out: 'Tensor') -> None:
+
+        def _backward_fn(out: "Tensor") -> None:
             assert out.grad is not None
             if self.grad is not None:
                 self.grad += (out.value > 0) * out.grad
 
         return Tensor(
             (self.value > 0) * self.value,
-            name='relu',
+            name="relu",
             _ancestors=[self],
             _backward_fn=_backward_fn,
         )
 
     def sigmoid(self):
         """Analog of `torch.Tensor.sigmoid()`."""
-        def _backward_fn(out: 'Tensor') -> None:
+
+        def _backward_fn(out: "Tensor") -> None:
             if self.grad is not None:
                 self.grad += (out.value * (1 - out.value)) * out.grad
 
         return Tensor(
             1 / (1 + np.exp(-self.value)),
-            name='sigmoid',
+            name="sigmoid",
             _ancestors=[self],
             _backward_fn=_backward_fn,
         )
 
-    def matmul(self, other: 'Tensor') -> 'Tensor':
+    def matmul(self, other: "Tensor") -> "Tensor":
         """Analog of `torch.Tensor.matmul()`."""
         return self.tensordot(other, 1)
 
-    def tensordot(self, other: 'Tensor', dims: int = 2) -> 'Tensor':
+    def tensordot(self, other: "Tensor", dims: int = 2) -> "Tensor":
         """Analog of `torch.Tensor.tensordot()`."""
-        def _backward_fn(out: 'Tensor') -> None:
+
+        def _backward_fn(out: "Tensor") -> None:
             assert out.grad is not None
             if self.grad is not None:
                 # Figure out how many dimensions to contract going backwards.
-                to_contract_ndim = (out.grad.ndim + other.value.ndim - self.grad.ndim) // 2
+                to_contract_ndim = (
+                    out.grad.ndim + other.value.ndim - self.grad.ndim
+                ) // 2
 
-                # Gradient update is tensor contraction of output gradient and other's tranpose.
-                self.grad += np.tensordot(out.grad, _transpose(other.value, to_contract_ndim), axes=to_contract_ndim)
+                # Gradient update is tensor contraction of output gradient and
+                # other's tranpose.
+                self.grad += np.tensordot(
+                    out.grad,
+                    _transpose(other.value, to_contract_ndim),
+                    axes=to_contract_ndim,
+                )
 
             if other.grad is not None:
                 # Figure out how many dimensions to contract going backwards.
-                to_contract_ndim = (out.grad.ndim + self.value.ndim - other.grad.ndim) // 2
+                to_contract_ndim = (
+                    out.grad.ndim + self.value.ndim - other.grad.ndim
+                ) // 2
 
-                # Gradient update is tensor contraction of self's transpose and output gradient.
-                other.grad += np.tensordot(_transpose(self.value, self.value.ndim - to_contract_ndim), out.grad, axes=to_contract_ndim)
+                # Gradient update is tensor contraction of self's transpose and
+                # output gradient.
+                other.grad += np.tensordot(
+                    _transpose(self.value, self.value.ndim - to_contract_ndim),
+                    out.grad,
+                    axes=to_contract_ndim,
+                )
 
         return Tensor(
             np.tensordot(self.value, other.value, axes=dims),
-            name='tensordot',
+            name="tensordot",
             _ancestors=[self, other],
             _backward_fn=_backward_fn,
         )
 
-    def permute(self, dims: Sequence[int]) -> 'Tensor':
+    def permute(self, dims: Sequence[int]) -> "Tensor":
         """Analog of `torch.Tensor.permute()`."""
-        def _backward_fn(out: 'Tensor') -> None:
+
+        def _backward_fn(out: "Tensor") -> None:
             assert out.grad is not None
             if self.grad is not None:
                 self.grad += _inverse_permute(out.grad, dims)
 
         return Tensor(
             np.transpose(self.value, axes=dims),
-            name='permute',
+            name="permute",
             _ancestors=[self],
             _backward_fn=_backward_fn,
         )
 
-    def __add__(self, other: 'Tensor' | ArrayLike) -> 'Tensor':
+    def __add__(self, other: "Tensor" | ArrayLike) -> "Tensor":
         _other = other if isinstance(other, Tensor) else Tensor(other)
 
-        def _backward_fn(out: 'Tensor') -> None:
+        def _backward_fn(out: "Tensor") -> None:
             assert out.grad is not None
             if self.grad is not None:
                 self.grad += _unbroadcast_gradient(out.grad, self.grad.shape)
             if _other.grad is not None:
-                _other.grad += _unbroadcast_gradient(out.grad, _other.grad.shape)
+                _other.grad += _unbroadcast_gradient(
+                    out.grad, _other.grad.shape
+                )
 
         return Tensor(
             self.value + _other.value,
-            name='+',
+            name="+",
             _ancestors=[self, _other],
             _backward_fn=_backward_fn,
         )
 
-    def __mul__(self, other: 'Tensor' | ArrayLike) -> 'Tensor':
+    def __mul__(self, other: "Tensor" | ArrayLike) -> "Tensor":
         _other = other if isinstance(other, Tensor) else Tensor(other)
 
-        def _backward_fn(out: 'Tensor') -> None:
+        def _backward_fn(out: "Tensor") -> None:
             if self.grad is not None:
-                self.grad += _unbroadcast_gradient(out.grad * _other.value, self.grad.shape)
+                self.grad += _unbroadcast_gradient(
+                    out.grad * _other.value, self.grad.shape
+                )
             if _other.grad is not None:
-                _other.grad += _unbroadcast_gradient(out.grad * self.value, _other.grad.shape)
+                _other.grad += _unbroadcast_gradient(
+                    out.grad * self.value, _other.grad.shape
+                )
 
         return Tensor(
             self.value * _other.value,
-            name='*',
+            name="*",
             _ancestors=[self, _other],
             _backward_fn=_backward_fn,
         )
 
-    def __pow__(self, other: float | int) -> 'Tensor':
+    def __pow__(self, other: float | int) -> "Tensor":
         return self.pow(other)
 
-    def __neg__(self): # -self
+    def __neg__(self):  # -self
         return self * -1
 
-    def __radd__(self, other): # other + self
+    def __radd__(self, other):  # other + self
         return self + other
 
-    def __sub__(self, other): # self - other
+    def __sub__(self, other):  # self - other
         return self + (-other)
 
-    def __rsub__(self, other): # other - self
+    def __rsub__(self, other):  # other - self
         return other + (-self)
 
-    def __rmul__(self, other): # other * self
+    def __rmul__(self, other):  # other * self
         return self * other
 
-    def __truediv__(self, other): # self / other
+    def __truediv__(self, other):  # self / other
         return self * other**-1
 
-    def __rtruediv__(self, other): # other / self
+    def __rtruediv__(self, other):  # other / self
         return other * self**-1
 
-    def __matmul__(self, other): # self @ other
+    def __matmul__(self, other):  # self @ other
         return self.matmul(other)
 
-    def __rmatmul__(self, other): # other @ self
+    def __rmatmul__(self, other):  # other @ self
         return other.matmul(self)
 
     def backward(self) -> None:
         """Performs back-propagation starting from the given tensor.
 
-        This method will accumulate gradients for the transitive closure of all ancestors in the tensor's computation
-        graph. Back-propagation must start from a scalar tensor (typically representing a loss), as all gradients
-        are assumed to be computed relative to a scalar loss function.
+        This method will accumulate gradients for the transitive closure of all
+        ancestors in the tensor's computation graph. Back-propagation must
+        start from a scalar tensor (typically representing a loss), as all
+        gradients are assumed to be computed relative to a scalar loss
+        function.
 
-        Only nodes where ``requires_grad`` has been set to True will have gradients accumulated into.
+        Only nodes where ``requires_grad`` has been set to True will have
+        gradients accumulated into.
 
-        Like the corresponding pytorch method, this method does not automatically zero-out gradient buffers.
-        It is the responsibility of callers to zero-out gradients (e.g., by setting ``my_tensor.grad = None``).
+        Like the corresponding pytorch method, this method does not
+        automatically zero-out gradient buffers. It is the responsibility of
+        callers to zero-out gradients (e.g., by setting ``my_tensor.grad =
+        None``).
         """
         # Make sure we're starting from a scalar.
         if self.ndim != 0:
-            raise ValueError('gradients can only be computed from scalars')
+            raise ValueError("gradients can only be computed from scalars")
 
         # Compute the sorted transitive closure of ancestors.
         nodes = self._topological_sort()
@@ -279,32 +327,39 @@ class Tensor:
             if n.grad is not None and n._backward_fn is not None:
                 n._backward_fn(n)
 
-    def _topological_sort(self) -> list['Tensor']:
+    def _topological_sort(self) -> list["Tensor"]:
         """Topologically sorts the ancestor graph.
 
-        The sort order is such that the root node (i.e., self) appears first, and all leaf nodes appear later.
+        The sort order is such that the root node (i.e., self) appears first,
+        and all leaf nodes appear later.
         """
         visited_list = []
         visited_set = set()
-        def _visit(node: 'Tensor') -> None:
+
+        def _visit(node: "Tensor") -> None:
             if node in visited_set:
                 return
             visited_set.add(node)
             for ancestor in node._ancestors:
                 _visit(ancestor)
             visited_list.append(node)
+
         _visit(self)
         visited_list.reverse()
         return visited_list
 
     def __str__(self) -> str:
-        return f'Tensor({self.value}, name=\'{self.name}\')'
+        return f"Tensor({self.value}, name='{self.name}')"
 
-def _unbroadcast_gradient(grad: np.ndarray, shape: Sequence[int]) -> np.ndarray:
+
+def _unbroadcast_gradient(
+    grad: np.ndarray, shape: Sequence[int]
+) -> np.ndarray:
     """Squashes gradients in order to account for broadcasting.
 
-    This method sums gradients along dimensions that would be broadcasted during some operation. For example,
-    if the target shape is a scalar, then this method should sum along all dimensions of the gradient and return
+    This method sums gradients along dimensions that would be broadcasted
+    during some operation. For example, if the target shape is a scalar, then
+    this method should sum along all dimensions of the gradient and return
     a scalar.
 
     Args:
@@ -314,7 +369,8 @@ def _unbroadcast_gradient(grad: np.ndarray, shape: Sequence[int]) -> np.ndarray:
     Returns:
         A numpy array with the target shape.
     """
-    # Prepend extra dimensions of size 1 to shape as necessary to match broadcast rules.
+    # Prepend extra dimensions of size 1 to shape as necessary to match
+    # broadcast rules.
     missing_dims = grad.ndim - len(shape)
     bcast_shape = ([1] * missing_dims) + list(shape)
 
@@ -327,11 +383,13 @@ def _unbroadcast_gradient(grad: np.ndarray, shape: Sequence[int]) -> np.ndarray:
     # Reshape to target shape.
     return squashed.reshape(shape)
 
+
 def _transpose(a: np.ndarray, dims: int) -> np.ndarray:
     """Transposes a set number of dimensions.
 
-    The last `dims` dimensions of the array are moved to the front of the array. For example, if the input
-    array has shape (5, 4, 3) and `dims` is 2, then the output array will have shape (4, 3, 5).
+    The last `dims` dimensions of the array are moved to the front of the
+    array. For example, if the input array has shape (5, 4, 3) and `dims` is 2,
+    then the output array will have shape (4, 3, 5).
 
     Args:
         a: The numpy array to transpose.
@@ -341,11 +399,13 @@ def _transpose(a: np.ndarray, dims: int) -> np.ndarray:
     axes = axes[-dims:] + axes[:-dims]
     return np.transpose(a, axes)
 
+
 def _inverse_permute(a: np.ndarray, dims: Sequence[int]) -> np.ndarray:
     """Performs an inverse permutation of axes.
 
-    This method is effectively the inverse of `np.transpose()`. That is, if you pass an array with `dims` set
-    as the axes to `np.transpose()`, you can recover the original array by calling this method on the output.
+    This method is effectively the inverse of `np.transpose()`. That is, if you
+    pass an array with `dims` set as the axes to `np.transpose()`, you can
+    recover the original array by calling this method on the output.
 
     Args:
         a: The numpy array to inverse-permute.
